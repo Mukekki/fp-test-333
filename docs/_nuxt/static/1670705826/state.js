25f0f787085b290cbf1f3647a0d38c1db2943d50
mutekki <dk@tfl.io>
2022-12-11 00:07:34 +0300

    update

--- docs/_nuxt/static/1670705826/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffp-test-333\u002F_nuxt\u002Fstatic\u002F1670705826",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ffp-test-333\u002F",assetsPath:"\u002Ffp-test-333\u002F_nuxt\u002F",cdnURL:a}}}}(null));
